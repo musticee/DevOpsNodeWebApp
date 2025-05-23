2a851e97d43ef6340c6a154b36510263f524ff8a
Mustapha Ceesay <mustapha.ceesay@gmx.ch>
2025-05-23 09:41:16 +0200

    lernjournal-10: Anpassung für GitHubActions

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Lernjournal-10: Docker Deployment');
+  res.send('Lernjournal-10: GitHub Actions!');
 });
 
 app.listen(PORT, HOST);
